47da670f03860316b524b643c3ef402d8f6e13c9
max <maksim.levental@gmail.com>
2015-01-19 22:24:45 -0500

    done

--- scanner/Scanner.java
+++ scanner/Scanner.java
@@ -10,29 +10,40 @@ import static cop5555sp15.TokenStream.Kind.*;
 public class Scanner {
 
     private enum State {
-	START, GOT_EQUALS, IDENT_PART, GOT_ZERO, DIGITS, EOF, GOTCR, SLASH, COMMENT, WHITESPACE, OPERATOR, SEPARATOR
+	START, IDENT_PART, DIGITS, EOF,  COMMENT, WHITESPACE, OPERATOR, SEPARATOR, STRING_LIT
     }
     private State state;
     public static final Set<Character> sepBegs = new HashSet<Character>(Arrays.asList('.' , ';' , ',' , '(' , ')' , '[' , ']', '{' , '}', ':', '?'));
     public static final Set<Character> opBegs = new HashSet<Character>(Arrays.asList('=' , '|' , '&' , '!' , '<' , '>' , '+' , '-' , '*' , '/' , '%' , '@'));
     public static final Set<Character> whiteSp = new HashSet<Character>(Arrays.asList('\n','\t','\r',' '));
+
+    public static final String[] keywordStrs = {"int","string","boolean","import","class","def","while","if","else","return","print","true","false","null"};
+    public static final Kind[] keywordKnds = {KW_INT, KW_STRING, KW_BOOLEAN, KW_IMPORT, KW_CLASS, KW_DEF, KW_WHILE, KW_IF, KW_ELSE, KW_RETURN, KW_PRINT,BL_TRUE, BL_FALSE,NL_NULL,};
+    public static final Map<String,Kind> keywords;
+    static{
+	keywords = new HashMap<String,Kind>();
+	for(int i=0; i<keywordStrs.length;++i)
+	    keywords.put(keywordStrs[i],keywordKnds[i]);
+    }
+
     //local references to TokenStream objects for convenience
     final TokenStream tStream;  //set in constructor
     private int index; // points to the next char to process during scanning, or if none, past the end of the array
     char ch;
     int lineNum;
+    int begOffset;
     public Scanner(TokenStream tStrm){
 	tStream = tStrm;
 	index = 0;
+	begOffset = 0;
 	lineNum = 1;
     }
    
     
     private char getch() throws IOException {
-	if(tStream.inputChars.length>=index+1)
-	    ch = tStream.inputChars[index++];
-	else
-	    ch = '\0';
+	if(tStream.inputChars.length>index) ch = tStream.inputChars[index];
+	else ch = '\0';
+	index++;
 	//System.out.println("getch:"+(int)ch);
 	return ch;
     }
@@ -42,8 +53,6 @@ public class Scanner {
 
 	state = State.START;
 	Token t = null;
-	int begOffset=0;
-
 	getch();
 	do { // loop terminates when a token is created 
 	    switch (state) { 
@@ -51,43 +60,24 @@ public class Scanner {
 		  either create a token or change state
 		*/
 	    case START:
-		begOffset = index;
+		begOffset = index-1;
 		//System.out.println("index:"+index);
 		//System.out.println((int)ch);
-		if(ch =='\0'){
-		    state = State.EOF;
-		    break;
-		}
-		else if(whiteSp.contains(ch)){
-		    state = State.WHITESPACE;
-		    break;
-		}
-		else if(opBegs.contains(ch)){
-		    state = State.OPERATOR; 
-		    break;
-		}
-		else if(sepBegs.contains(ch)){
-		    state = State.SEPARATOR;
-		    break;
-		}
-		else{
-		    switch(ch){
-		    }
-		    getch();
-		}
+		if(ch=='\0') t = makeDefaTok(EOF);
+		else if(whiteSp.contains(ch)) state = State.WHITESPACE;
+		else if(opBegs.contains(ch)) state = State.OPERATOR; 
+		else if(sepBegs.contains(ch)) state = State.SEPARATOR;
+		else if(Character.isDigit(ch)) state = State.DIGITS;
+		else if(Character.isJavaIdentifierStart(ch)) state = State.IDENT_PART;
+		else if(ch=='"') state = State.STRING_LIT;
+		else t = makeDefaTok(ILLEGAL_CHAR);
 		//System.out.println(index);
 		break; // end of state START
-	    case EOF:
-		t = makeDefaTok(EOF,begOffset);
-		break;
 	    case WHITESPACE:
-		if(ch==' ' || ch=='\t'){ //blank space, getch, then go back to start
-		    getch();
-		}
+		if(ch==' ' || ch=='\t') getch(); //blank space, getch, then go back to start
 		else if(ch=='\r'){ // CR, getch next char, if not \n then incr lineNum and leave next otherwise, increment and getch again
 		    getch();
-		    if(ch == '\n')
-			getch();
+		    if(ch == '\n') getch();
 		    lineNum++;	    
 		}
 		else {// if(ch== '\n'):
@@ -97,66 +87,128 @@ public class Scanner {
 	        state = State.START;
 	        break;
 	    case OPERATOR:
-		if (ch=='/'){
+		//System.out.println(ch);
+		if(ch=='='){
 		    getch();
-		    switch(ch){
-		    case '*':
+		    if(ch=='=') t = makeDefaTok(EQUAL);
+		    else t = makePeekTok(ASSIGN);
+		} 
+		else if(ch=='|') t = makeDefaTok(BAR);
+		else if(ch=='!'){
+		    getch();
+		    if(ch=='=') t = makeDefaTok(NOTEQUAL);
+		    else t = makePeekTok(NOT);
+		}
+		else if(ch=='<'){
+		    getch();
+		    if(ch=='=') t = makeDefaTok(LE);
+		    else if(ch=='<') t = makeDefaTok(LSHIFT);
+		    else t = makePeekTok(LT);
+		}
+		else if(ch=='>'){
+		    getch();
+		    if(ch=='=') t = makeDefaTok(GE);
+		    else if(ch=='>') t = makeDefaTok(RSHIFT);
+		    else t = makePeekTok(GT);
+		}
+		else if(ch=='+') t = makeDefaTok(PLUS);
+		else if(ch=='-'){
+		    getch();
+		    if(ch=='>') t = makeDefaTok(ARROW);
+		    else t = makePeekTok(MINUS);
+		}
+		else if(ch=='*') t = makeDefaTok(TIMES);
+		else if(ch=='/'){
+		    getch();
+		    if(ch== '*')
 			state = State.COMMENT;
-			break;
-		    default:
-			t = tStream.new Token(DIV, begOffset, index-1, lineNum);
-			//don't eat the character
-			state = State.START;
-		    }
-		    break;
+		    else
+			t = makePeekTok(DIV);//tStream.new Token(DIV, begOffset, --index, lineNum);
 		}
+		else if(ch=='%') t = makeDefaTok(MOD);
+		else t = makeDefaTok(AT);
+		break;
 	    case SEPARATOR:
-		if(ch=='.'){
-		    //tok.makeDefaTok(DOT);//
-		    t = makeDefaTok(DOT, begOffset);
+		if(ch=='.'){ 
+		    getch();
+		    if(ch=='.')	t = makeDefaTok(RANGE);
+		    else t = makePeekTok(DOT);
 		}
-
-		///////////////////////////////////////////////
-		//implement rest of separators
-		//
-		///////////////////////////////////////////////
-
-
-
-
-
+		else if(ch==';') t = makeDefaTok(SEMICOLON);
+		else if(ch==',') t = makeDefaTok(COMMA);
+		else if(ch=='(') t = makeDefaTok(LPAREN);
+		else if(ch==')') t = makeDefaTok(RPAREN);
+		else if(ch=='[') t = makeDefaTok(LSQUARE);
+		else if(ch==']') t = makeDefaTok(RSQUARE);
+		else if(ch=='{') t = makeDefaTok(LCURLY);
+		else if(ch=='}') t = makeDefaTok(RCURLY);
+		else if(ch==':') t = makeDefaTok(COLON);
+		else t = makeDefaTok(QUESTION);
+		break;
 	    case COMMENT:
 		boolean inComment = true;
 		getch(); // have eaten /*, char right after '/*' in ch
-		//System.out.println(ch+" "+index);
-		do {
-		    switch(ch){
-		    case '\n':
-			lineNum++;
+		
+		while(true){    
+		    //System.out.println((int)ch);
+		    if(ch=='\0'){ //unterminated comment
+			t = makePeekTok(UNTERMINATED_COMMENT);
 			break;
-		    case '\r':
+		    }
+		    else if(ch=='\n'){
+			lineNum++;
+			getch();
+		    }
+		    else if(ch=='\r'){
 			getch();
 			if(ch=='\n'){
 			    lineNum++;
-			    break;
+			    getch();
 			}
 			else{
 			    lineNum++;
 			    continue;
 			}
-		    case '*':
+		    }
+		    else if(ch=='*'){
 			getch();
 			if(ch=='/'){
 			    state = State.START;
-			    inComment = false;
+			    getch();
 			    break;
 			}
-		    default:
-			//do nada
 		    }
-		    getch();
-		} while(inComment);
+		    else getch();
+		    
+		}
 		break;
+	    case DIGITS:
+		if(ch=='0') t = makeDefaTok(INT_LIT);
+		else{
+		    while(true){
+			getch();
+			if(ch<48 || 57<ch) break;
+		    }
+		    t = makePeekTok(INT_LIT);
+		}
+		break;
+	    case IDENT_PART:
+		while(true){
+		    getch();
+		    if(!Character.isJavaIdentifierStart(ch) && !Character.isDigit(ch)) break;
+		}
+		t = makePeekTok(IDENT);
+		if(keywords.containsKey(t.getText()))
+		    t = tStream.new Token(keywords.get(t.getText()), t.beg, t.end, t.lineNumber);
+		break;
+	    case STRING_LIT:
+		//System.out.println("stringlit"+ch);
+		while(getch() != '"')
+		    if(ch=='\0'){
+			t = makeDefaTok(UNTERMINATED_STRING);
+			return t;
+		    }
+		t = makeDefaTok(STRING_LIT);
 	    default:
 		assert false : "should not reach here";
 	    }// end of switch(state)
@@ -177,10 +229,14 @@ public class Scanner {
 	}
     }
 
-    private Token makeDefaTok(Kind typ,int begOffset){
+    private Token makeDefaTok(Kind typ){
 	return tStream.new Token(typ,begOffset,index,lineNum);
     }
 
+    //for 1 char look ahead have to reset index if diff token
+    private Token makePeekTok(Kind typ) throws IOException {
+	return tStream.new Token(typ,begOffset,--index,lineNum);
+    }
    
 }
 
