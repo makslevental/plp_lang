4e3c96275ea1a187771e21ee45c51c99fee8cf84
Maksim Levental <maksim.levental@gmail.com>
2020-05-20 09:26:40 -0400

    Update and rename compile_script.sh to compile_script123.sh

--- compile_script123.sh
+++ compile_script123.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$CLASSPATH" ]; then 
-    export CLASSPATH=/home/maksim/Desktop/plp_lang/parser/:/home/maksim/Desktop/plp_lang/ast/:/home/maksim/Desktop/plp_lang/scanner/:/usr/share/java/junit-4.12.jar:/usr/share/java/hamcrest-core-1.3.jar:/home/maksim/Desktop/plp_lang/type_checking_code_generation/asm-5.0.3.jar:/home/maksim/Desktop/plp_lang/type_checking_code_generation/asm-util-5.0.3.jar:/home/maksim/Desktop/plp_lang/type_checking_code_generation_2/:/home/maksim/Desktop/plp_lang/codelet/;
+    export CLASSPATH=/home//Desktop/plp_lang/parser/:/home//Desktop/plp_lang/ast/:/home//Desktop/plp_lang/scanner/:/usr/share/java/junit-4.12.jar:/usr/share/java/hamcrest-core-1.3.jar:/home//Desktop/plp_lang/type_checking_code_generation/asm-5.0.3.jar:/home//Desktop/plp_lang/type_checking_code_generation/asm-util-5.0.3.jar:/home//Desktop/plp_lang/type_checking_code_generation_2/:/home//Desktop/plp_lang/codelet/;
 fi
 
 
